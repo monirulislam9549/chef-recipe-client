c951d1315cba6c228ec0d31c6d1ed339deaccb45
monirulislam9549 <monirulislam9549@gmail.com>
2023-05-03 10:15:59 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from './components/Shared/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Shared/Footer';
 
 const App = () => {
   return (
     <div >
-      <h1 className='text-red-500 text-5xl'>testing</h1>
+      <Navbar></Navbar>
+      <div className='min-h-[calc(100vh-136px)]'><Outlet></Outlet></div>
+      <Footer></Footer>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import Home from './components/Home/Home'
+import Blog from './components/Blog/Blog'
+import About from './components/About/About'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>
+      },
+      {
+        path: "about",
+        element: <About></About>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
+
+
