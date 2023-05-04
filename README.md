4ceae2280bdca942978a1dc9c6e88f1d15ce6c1e
monirulislam9549 <monirulislam9549@gmail.com>
2023-05-04 23:19:24 +0600

    add to favourite button added

---
+++ README.md
@@ -0,0 +1,25 @@
+## About My Project
+* This website data comes from server side.
+* This Project build with authentication email-password, googleSignIn, GithubSignIn.
+* This Project is About to Chef website.
+* User Cannot View Recipe without login.
+* This is our 10th assignment.
+
+## This is Our Assignment-10
+### In This Project I used Below this Packages
+* Hero Icons
+* Font Awesome
+
+### For Animation
+* Lottie React
+
+### FrameWork
+* React JS
+* Express JS
+
+
+### CSS FrameWork
+* TailWind CSS
+
+### CSS Component
+* Daisy UI
--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { HandThumbUpIcon } from '@heroicons/react/24/solid'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const RecipeDetails = () => {
     const [data, setData] = useState({})
-    const [recipe, setRecipe] = useState([])
+    // const [recipe, setRecipe] = useState([])
+    const [selected, setSelected] = useState(false)
     const { id } = useParams()
     const {
         chefName, yearsOfExperience, numberOfRecipes, likes, bio, recipeTitle, chefPicture, ingredients,
     } = data
+
+    const handleFavourite = (event) => {
+        setSelected(event.target)
+        toast('Add To Favourite')
+    }
+
     console.log(data.ingredients);
     useEffect(() => {
         fetch(`http://localhost:5000/recipe`)
@@ -17,33 +25,39 @@ const RecipeDetails = () => {
             .then(data => {
                 const unique = data.find(match => match.id == id)
                 setData(unique)
-                setRecipe(unique.recipes)
+                // setRecipe(unique.recipes)
             })
     }, [])
 
 
     // console.log(data);
     return (
-        <div className="bg-base-100 shadow-xl">
+        <div className="bg-base-100 shadow-xl px-10">
             <div>
-                <figure><img className='w-80' src={chefPicture} alt="Album" /></figure>
+                <img className='w-80' src={chefPicture} alt="Album" />
                 <div className=''>
                     <p>{chefName}</p>
                     <p>{bio}</p>
                 </div>
             </div>
+            <div className='flex items-center'>
+                <HandThumbUpIcon className='fill-blue-700 w-8 mr-1'></HandThumbUpIcon>
+                <p className='font-semibold text-xl'>Likes: {likes}</p>
+            </div>
+            <p className='font-semibold text-xl'>Recipes: {numberOfRecipes} Item</p>
+            <p className='font-semibold text-xl'>Experience: {yearsOfExperience} year<sup>+</sup> </p>
             <div className="card-body">
                 <h2 className="card-title">{recipeTitle}</h2>
 
                 {
-                    recipe && recipe.map((r, index) => (
-                        <li key={index}>{r}</li>
+                    ingredients && ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
                     ))
                 }
 
-                <div className="card-actions justify-end">
-                    {/* <button className="btn btn-primary">Listen</button> */}
-                </div>
+            </div>
+            <div><button disabled={selected} onClick={handleFavourite} class="btn btn-primary">Favourite</button>
+                <ToastContainer></ToastContainer>
             </div>
         </div>
     );
